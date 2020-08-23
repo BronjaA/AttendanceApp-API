c9aacde8df32f210dedad4c05359fa21a4f36254
Armin Bronja <42872130+BronjaA@users.noreply.github.com>
2020-08-23 13:49:34 +0200

    Update activities.js

--- routes/subjects/activities.js
+++ routes/subjects/activities.js
@@ -19,4 +19,21 @@ router.post('/create', async (req, res) => {
     }
 });
 
+router.post('/getByID', async (req, res) => {
+    try{
+        var activities = [];
+        for(let i=0; i < req.body.actID.length; i++)
+        {
+            activities.push(await Activity.findOne({_id: req.body.actID[i]}));
+        }
+
+        if(activities)
+        {
+            res.status(200).send(activities);
+        }
+    }catch(err){
+        res.status(400).send(err);
+    }
+})
+
 module.exports = router;
